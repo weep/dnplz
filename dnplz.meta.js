f4ab17277898711f9ee77c42d0c061c533305848
Oscar Holmér <weep.133@gmail.com>
2016-06-19 21:59:56 +0200

    0.2 author info Added author and namespace

--- dnplz.meta.js
+++ dnplz.meta.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         DN "paywall"
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    http://weep.se/
+// @version      0.2
 // @description  try to take over the world!
-// @author       You
+// @author       Weep
 // @updateURL	 https://raw.githubusercontent.com/weep/dnplz/master/dnplz.meta.js
 // @downloadURL  https://raw.githubusercontent.com/weep/dnplz/master/dnplz.user.js
-// @match        http://www.dn.se/*
+// @match        http*://www.dn.se/*
+// @match        http*://dn.se/*
 // @grant        none
 // ==/UserScript==
