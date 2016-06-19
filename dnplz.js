5ba92b535689e9d80a867e97c5e9ec532041e795
Oscar Holmér <weep@weep.se>
2016-06-19 21:34:01 +0200

    Actually add the code

---
+++ dnplz.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         DN "paywall"
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://www.dn.se/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    dnplz();
+})();
+
+function dnplz(){
+    addGlobalStyle('.article__body--mask .article__lead:after { box-shadow: 0 0  !important; } ');
+    addGlobalStyle('.paywall { display: none !important; ');
+    addGlobalStyle('.article__body--mask .article__premium-content { display: block !important; ');
+}
+
+ function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
