b6849802a3986527f621c1af0a12542480b63ac6
weep <weep@weep.se>
2016-06-19 21:45:59 +0200

    Added meta file. Renamed to user.js

---
+++ dnplz.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         DN "paywall"
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @updateURL	 https://raw.githubusercontent.com/weep/dnplz/master/dnplz.meta..js
+// @downloadURL  https://raw.githubusercontent.com/weep/dnplz/master/dnplz.user.js
+// @match        http://www.dn.se/*
+// @grant        none
+// ==/UserScript==
