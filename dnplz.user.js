4235adfe28b100dbfb5a1da6c9a90e4c6227b86a
weep <weep@weep.se>
2016-06-19 21:47:29 +0200

    updated meta in user.js

--- dnplz.user.js
+++ dnplz.user.js
@@ -4,6 +4,8 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
+// @updateURL	 https://raw.githubusercontent.com/weep/dnplz/master/dnplz.meta..js
+// @downloadURL  https://raw.githubusercontent.com/weep/dnplz/master/dnplz.user.js
 // @match        http://www.dn.se/*
 // @grant        none
 // ==/UserScript==
