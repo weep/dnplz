a4bb759bf0d29dc705f46d3e5dc0d293bd65bf2b
Oscar Holmér <weep.133@gmail.com>
2016-06-19 21:52:46 +0200

    "." ...

--- dnplz.meta.js
+++ dnplz.meta.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @updateURL	 https://raw.githubusercontent.com/weep/dnplz/master/dnplz.meta..js
+// @updateURL	 https://raw.githubusercontent.com/weep/dnplz/master/dnplz.meta.js
 // @downloadURL  https://raw.githubusercontent.com/weep/dnplz/master/dnplz.user.js
 // @match        http://www.dn.se/*
 // @grant        none
