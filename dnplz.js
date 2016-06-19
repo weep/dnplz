c209cca23de52391275f2d7be130487f8b149e46
Oscar Holmér <weep@weep.se>
2016-06-19 21:39:23 +0200

    some scoping

--- dnplz.js
+++ dnplz.js
@@ -11,21 +11,21 @@
 (function() {
     'use strict';
 
+	function dnplz(){
+		addGlobalStyle('.article__body--mask .article__lead:after { box-shadow: 0 0  !important; } ');
+		addGlobalStyle('.paywall { display: none !important; ');
+		addGlobalStyle('.article__body--mask .article__premium-content { display: block !important; ');
+	}
+
+	 function addGlobalStyle(css) {
+		var head, style;
+		head = document.getElementsByTagName('head')[0];
+		if (!head) { return; }
+		style = document.createElement('style');
+		style.type = 'text/css';
+		style.innerHTML = css;
+		head.appendChild(style);
+	}
+
     dnplz();
 })();
-
-function dnplz(){
-    addGlobalStyle('.article__body--mask .article__lead:after { box-shadow: 0 0  !important; } ');
-    addGlobalStyle('.paywall { display: none !important; ');
-    addGlobalStyle('.article__body--mask .article__premium-content { display: block !important; ');
-}
-
- function addGlobalStyle(css) {
-    var head, style;
-    head = document.getElementsByTagName('head')[0];
-    if (!head) { return; }
-    style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = css;
-    head.appendChild(style);
-}
